9372beac538f5f362ae0e31aae2cb7709fdee0a2
Y Nathan M <97424103+Yakobus-Mardi@users.noreply.github.com>
2023-02-01 15:47:40 +0700

    change Guest to Away

--- Basketball Scoreboard/index.js
+++ Basketball Scoreboard/index.js
@@ -1,32 +1,28 @@
 const scoreBoard = document.querySelectorAll("#score-board");
-// const btnIncrement1 = document.querySelectorAll("#incrementOne");
-// const btnIncrement2 = document.querySelectorAll("#incrementTwo");
-// const btnIncrement3 = document.querySelectorAll("#incrementThree");
 const resetBtn = document.querySelectorAll("#reset");
 
 let homeScore = 0;
-let guestScore = 0;
+let awayScore = 0;
 
 scoreBoard[0].textContent = 0;
 scoreBoard[1].textContent = 0;
-// for (const board of scoreBoard) board.textContent = 0;
 
 function incrementHome(n) {
-   homeScore += n;
-   scoreBoard[0].textContent = homeScore;
-} 
+  homeScore += n;
+  scoreBoard[0].textContent = homeScore;
+}
 
-function incrementGuest(n) {
-    guestScore += n;
-    scoreBoard[1].textContent = guestScore;
+function incrementAway(n) {
+  awayScore += n;
+  scoreBoard[1].textContent = awayScore;
 }
 
 function resetButtonHome() {
-    scoreBoard[0].textContent = 0;
-    homeScore = 0;
+  scoreBoard[0].textContent = 0;
+  homeScore = 0;
 }
 
-function resetButtonGuest() {
-    scoreBoard[1].textContent = 0;
-    guestScore = 0;
+function resetButtonAway() {
+  scoreBoard[1].textContent = 0;
+  awayScore = 0;
 }
